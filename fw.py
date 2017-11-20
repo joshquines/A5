7724319024a102b69252dddc22592c937f9e305a
SteveLeong <Steven@Stevens-MBP.hitronhub.home>
2017-11-19 17:26:52 -0700

    rules handled

--- fw.py
+++ fw.py
@@ -4,6 +4,55 @@ import os
 # Array to store rules as dictionaries
 RULES = []
 
+def validRule(rule, direction, action, ip, ports, established, count):
+    valid = True
+
+    # minimum fields is 4, maximum fields is 5
+    if len(rule) < 4 or len(rule) > 5:
+        print("Error: Malformed rule detected on line " + count + " - Incorrect Fields")
+        print("Valid Rule: <direction> <action> <ip> <port> [flag]")
+        return False
+
+    # check valid direction
+    if direction not in ("in", "out"):
+        print("Error: Malformed rule detected on line " + count + " - Invalid Direction")
+        print("Allowed values for <direction>: \"in\" and \"out\" ")
+        valid = False
+
+    # check valid action
+    if action not in ("accept", "drop", "reject"):
+        print("Error: Malformed rule detected on line " + count + " - Invalid Action")
+        print("Allowed values for <action>: \"accept\", \"drop\", and \"reject\" ")
+        valid = False
+    
+    # check valid IP notation
+    if ip == "*":
+        pass
+    else:
+        octets = ip.split(".")
+        cidr = octets[3].split("/")
+        if len(octets) != 4 or cidr != 2:
+            print("Error: Malformed rule detected on line " + count + " - IP must be in CIDR notation: a.b.c.d/xx")
+            valid = False
+    
+    # check valid ports
+    for p in ports:
+        if p == "*":
+            pass
+        else:
+            if p not in range(0, 65535):
+                print("Error: Malformed rule detected on line " + count + " - A port is not in range (0-65535)")
+                valid = False
+
+    # check if [flag] is established
+    if established == True:
+        if rule[-1] != "established":
+            print("Error: Malformed rule detected on line " + count + " - Allowed value for [flag]: \"established\"")
+            valid = False
+            
+    return valid
+
+
 def setRules(filename):
     success = True
     # check if filename is a file
@@ -16,20 +65,23 @@ def setRules(filename):
                 for line in rfile:
                     count += 1
                     lineContent = line.split()
+
+                    #split contents
+                    direction = lineContent[0]
+                    action = lineContent[1]
+                    ip = lineContent[2]
+                    port = lineContent[3]
+                    ports = port.split(",")
+
+                    # check established (if ports is the last field established is false)
+                    if port == lineContent[-1]:
+                        established = False
+                    else:
+                        established = True
+
                     # if the rule is valid
-                    if validRule(lineContent):
+                    if validRule(lineContent, direction, action, ip, ports, established, count):
                         # turn it into a dictionary
-                        direction = lineContent[0]
-                        action = lineContent[1]
-                        ip = lineContent[2]
-                        port = lineContent[3].split(",")
-
-                        # check established
-                        if port == lineContent[-1]:
-                            established = 0
-                        else:
-                            established = 1
-
                         rule = { 'direction': direction,
                                  'action': action,
                                  'ip': ip
@@ -42,10 +94,14 @@ def setRules(filename):
                         global RULES
                         RULES.append(rule)
                     else:
-                        print("Error: Invalid rule has been found")
                         success = False
             rfile.close()
             return
+        except:
+            print("Error: Could not open " + filename)
+            return False
+    
+    return success
 
 
 if __name__ == "__main__":
@@ -56,6 +112,9 @@ if __name__ == "__main__":
         if not setRules(filename):
             print("Error: Unable to process rules from configuration file")
             sys.exit()
+
+        for line in sys.stdin:
+            
         
 
 
